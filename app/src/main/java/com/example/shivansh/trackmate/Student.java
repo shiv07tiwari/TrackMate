07ea0aaef9d2092e88e04f8d3cb25c8f5aab9c91
Shivansh Tiwari <shivansh16tiwari@gmail.com>
2020-10-09 14:03:18 +0530

    Update Student.java

--- app/src/main/java/com/example/shivansh/trackmate/Student.java
+++ app/src/main/java/com/example/shivansh/trackmate/Student.java
@@ -10,34 +10,4 @@ public class Student {
         this.roll = roll;
         this.type = type;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRoll() {
-        return roll;
-    }
-
-    public void setRoll(String roll) {
-        this.roll = roll;
-    }
-    public void setType(Integer type) {
-        this.type = type;
-    }
-    public int getType() {
-        return type;
-    }
 }
